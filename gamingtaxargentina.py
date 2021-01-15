009084f3291279fa1e5924a210c0d8e6ab88bcea
KevinKupervaser <77300604+KevinKupervaser@users.noreply.github.com>
2021-01-15 20:26:04 -0300

    Update gamingtaxargentina.py

--- gamingtaxargentina.py
+++ gamingtaxargentina.py
@@ -5,6 +5,7 @@ def gamingtaxargentina():
     print("This program calculates the value of a videogame + taxes in Argentina (MicrosoftStore).")
     game_value = float(input("Enter the value of the game (ARS): "))
     game_value = (game_value * 164.001) / 100
+    game_value = round(game_value, 2)
     print("The price of the game + taxes is: ", game_value)
 
 gamingtaxargentina()
